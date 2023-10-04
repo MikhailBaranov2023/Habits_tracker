e324adee477f0047fc4566e47464429977d3bfd9
mikhailbaranov <problemaprod@gmail.com>
2023-10-04 18:35:51 +0100

    create User model

--- config/settings.py
+++ config/settings.py
@@ -40,8 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
 
     'habits',
-    'users',
-
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -125,4 +124,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODELS = 'users.User'
+AUTH_USER_MODEL = 'users.User'
